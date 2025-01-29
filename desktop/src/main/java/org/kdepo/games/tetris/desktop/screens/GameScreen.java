0d778db823a782f1dc077d700387dd7418212a4a
k8en <enklause@yandex.ru>
2025-01-29 16:38:34 +0400

    Added lines removing, statistics collecting

--- desktop/src/main/java/org/kdepo/games/tetris/desktop/screens/GameScreen.java
+++ desktop/src/main/java/org/kdepo/games/tetris/desktop/screens/GameScreen.java
@@ -11,6 +11,7 @@ import org.kdepo.graphics.k2d.MouseHandler;
 import org.kdepo.graphics.k2d.screens.AbstractScreen;
 
 import java.awt.*;
+import java.util.List;
 import java.util.Map;
 
 public class GameScreen extends AbstractScreen {
@@ -32,14 +33,17 @@ public class GameScreen extends AbstractScreen {
     private long nextStepTimer;
     private final int nextStepDelay;
 
+    private int linesCount;
+    private int score;
+
     public GameScreen() {
         this.name = Constants.Screens.GAME;
 
         fieldScreenPositionX = 10;
-        fieldScreenPositionY = 10 + 100;
+        fieldScreenPositionY = 10;
 
         fieldPreviewScreenPositionX = 350;
-        fieldPreviewScreenPositionY = 10 + 100;
+        fieldPreviewScreenPositionY = 10;
 
         nextStepDelay = 1000;
         nextStepTimer = System.currentTimeMillis() + nextStepDelay;
@@ -58,6 +62,9 @@ public class GameScreen extends AbstractScreen {
 
         currentFigureFieldCellX = 3;
         currentFigureFieldCellY = 0;
+
+        linesCount = 0;
+        score = 0;
     }
 
     @Override
@@ -70,6 +77,12 @@ public class GameScreen extends AbstractScreen {
 
             } else {
                 FieldUtils.mergeData(field.getData(), currentFigure.getData(), currentFigureFieldCellX, currentFigureFieldCellY);
+
+                List<Integer> completedLinesIndexes = FieldUtils.getCompletedLinesIndexes(field.getData());
+                if (!completedLinesIndexes.isEmpty()) {
+                    updateStatistics(completedLinesIndexes.size());
+                    FieldUtils.removeLines(field.getData(), completedLinesIndexes);
+                }
 
                 if (FieldUtils.isFieldOverflow(field.getData())) {
                     System.out.println("Game Over");
@@ -112,5 +125,21 @@ public class GameScreen extends AbstractScreen {
     @Override
     public void dispose() {
 
+    }
+
+    private void updateStatistics(int linesCount) {
+        if (linesCount <= 0 || linesCount > 4) {
+            throw new RuntimeException("Wrong lines count: " + linesCount);
+        }
+        this.linesCount = this.linesCount + linesCount;
+        if (linesCount == 1) {
+            score = score + 100;
+        } else if (linesCount == 2) {
+            score = score + 300;
+        } else if (linesCount == 3) {
+            score = score + 600;
+        } else if (linesCount == 4) {
+            score = score + 1000;
+        }
     }
 }
--- desktop/src/main/java/org/kdepo/games/tetris/desktop/utils/FieldUtils.java
+++ desktop/src/main/java/org/kdepo/games/tetris/desktop/utils/FieldUtils.java
@@ -2,6 +2,10 @@ package org.kdepo.games.tetris.desktop.utils;
 
 import org.kdepo.games.tetris.desktop.Constants;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FieldUtils {
 
     public static void printField(int[][] fieldData) {
@@ -87,5 +91,35 @@ public class FieldUtils {
             }
         }
         return false;
+    }
+
+    public static List<Integer> getCompletedLinesIndexes(int[][] data) {
+        List<Integer> linesIndexes = new ArrayList<>();
+        for (int row = 0; row < data.length; row++) {
+            boolean isLineCompleted = true;
+            for (int column = 0; column < data[row].length; column++) {
+                if (data[row][column] == 0) {
+                    isLineCompleted = false;
+                    break;
+                }
+            }
+            if (isLineCompleted) {
+                linesIndexes.add(row);
+            }
+        }
+        return linesIndexes;
+    }
+
+    public static void removeLines(int[][] data, List<Integer> linesIndexes) {
+        for (Integer lineIndex : linesIndexes) {
+            for (int row = lineIndex; row > 0; row--) {
+                for (int column = 0; column < data[row].length; column++) {
+                    data[row][column] = data[row - 1][column];
+                }
+            }
+        }
+
+        // Clear first row
+        Arrays.fill(data[0], 0);
     }
 }
