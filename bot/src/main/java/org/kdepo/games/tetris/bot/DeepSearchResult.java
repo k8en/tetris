58fc6afd49d8c6349c9364b119399c2093f4104f
k8en <enklause@yandex.ru>
2025-02-03 23:04:24 +0500

    Flush

---
+++ bot/src/main/java/org/kdepo/games/tetris/bot/DeepSearchResult.java
@@ -0,0 +1,23 @@
+package org.kdepo.games.tetris.bot;
+
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.evaluation.classification.Evaluation;
+
+public class DeepSearchResult {
+
+    private final Evaluation evaluation;
+    private final MultiLayerNetwork model;
+
+    public DeepSearchResult(Evaluation evaluation, MultiLayerNetwork model) {
+        this.evaluation = evaluation;
+        this.model = model;
+    }
+
+    public Evaluation getEvaluation() {
+        return evaluation;
+    }
+
+    public MultiLayerNetwork getModel() {
+        return model;
+    }
+}
